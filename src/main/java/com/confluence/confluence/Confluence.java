86cf1fa61a9d3def6f0c683660b6eb46e82a2f12
electroreal1 <155461193+electroreal1@users.noreply.github.com>
2025-12-06 15:05:00 +0200

    started Monthly Blade tool tier and a custom constructor to add tool tiers in quite an easy way

--- src/main/java/com/confluence/confluence/Confluence.java
+++ src/main/java/com/confluence/confluence/Confluence.java
@@ -1,33 +1,15 @@
 package com.confluence.confluence;
 
-import com.confluence.confluence.config.Config;
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import com.confluence.confluence.modules.kolras.items.KolrasToolTiers;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Confluence.MODID)
@@ -46,7 +28,7 @@ public class Confluence
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
+        KolrasToolTiers.init();
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
 //        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
